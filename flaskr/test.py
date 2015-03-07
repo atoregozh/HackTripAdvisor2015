6f0d044fa851c557d185c117a738612604957e8d
crackercracked <you@example.com>
2015-03-07 08:37:44 -0500

    changed geo api

--- flaskr/test.py
+++ flaskr/test.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template, json, url_for
+from flask import Flask, render_template, json, url_for, request
 import json
 from pygeocoder import Geocoder
-
+import useInstaTrip
+from urllib2 import urlopen
+import time
+import requests
+from geopy.geocoders import Nominatim
 app = Flask(__name__)
 
 def get_elements(img):
 		return [img["latitude"], img["longitude"], img["timestamp"].encode("utf-8"), img["thumbnail"].encode("utf-8")]
+
+def getCity(coordinate):
+    url = "http://api.tripadvisor.com/api/partner/2.0/map/" + str(coordinate[0]) + "," + str(coordinate[1]) + "," + "/geos?key=HackTripAdvisor-ade29ff43aed"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        city = data["data"][0]["name"]
+        return city
+    return None
 
 def city(coordinate):
 	place = Geocoder.reverse_geocode(coordinate[0], coordinate[1])
@@ -15,14 +29,22 @@ def city(coordinate):
 		return place[0].city
 
 @app.route('/')
+def login():
+	return render_template("login.html")
+	
+
+@app.route('/world_map', methods=['GET'])
 def world_map():
-		raw_data = open("igram.json", "r")
-		data = json.load(raw_data)
+		username = request.args.get("igid")
+		data = useInstaTrip.useInsta(username)
 		locations = {}
 		images_by_city = {}
 		for img in data:
+			if "latitude" not in img or "longitude" not in img:
+				continue
 			coordinate = [img["latitude"], img["longitude"]]
-			c = city(coordinate)
+			#c = "boston" #city(coordinate)
+			c = getCity(coordinate)
 			if c:
 				c_name = c.encode("utf-8")
 				locations[c_name] = coordinate
@@ -33,4 +55,5 @@ def world_map():
 		return render_template("world_map.html", locations=locations, all_images=images_by_city)
 
 if __name__ == '__main__':
-    app.run()
+		app.debug = True
+		app.run()
--- flaskr/useInstaTrip.py
+++ flaskr/useInstaTrip.py
@@ -7,21 +7,20 @@ import igram as insta
 import copy
 
 def useInsta(insta_userid):
-    userid = copy.copy(insta_userid)
-    uid = insta.get_ig_userid(userid)
-    photo_list = insta.get_recent_photos(uid)
+	userid = copy.copy(insta_userid)
+	uid = insta.get_ig_userid(userid)
+	photo_list = insta.get_recent_photos(uid)
 
-    for photo in photo_list:
-        lat = photo['latitude']
-        lon = photo['longitude']
-        name = photo['placename']
+	for photo in photo_list:
+		lat = photo['latitude']
+		lon = photo['longitude']
+		name = photo['placename']
 
-        location_id = trip.getAttractions(lat,lon,name)
-        reviews_and_urls = trip.getReviews(location_id)
-        
-        photo.update(reviews_and_urls)
-        
-        print photo
+		location_id = trip.getAttractions(lat,lon,name)
+		reviews_and_urls = trip.getReviews(location_id)
+		
+		photo.update(reviews_and_urls)
+	return photo_list
 
 def main():
     #TEST
