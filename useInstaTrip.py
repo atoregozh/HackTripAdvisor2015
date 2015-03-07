f1318b25c919d360c5e44aeadda1988ff5b50ad0
atoregozh <toregozh@gmail.com>
2015-03-07 11:57:11 -0500

    added More links

---
+++ useInstaTrip.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# ordering imports
+
+# self written module
+import trip
+import igram as insta
+import copy
+
+def useInsta(insta_userid):
+    userid = copy.copy(insta_userid)
+    uid = insta.get_ig_userid(userid)
+    photo_list = insta.get_recent_photos(uid)
+
+    for photo in photo_list:
+        lat = photo['latitude']
+        lon = photo['longitude']
+        name = photo['placename']
+
+        location_id = trip.getAttractions(lat,lon,name)
+        reviews_and_urls = trip.getReviews(location_id)
+        
+        photo.update(reviews_and_urls)
+        
+    return photo_list
+
+def main():
+    #TEST
+    userid = "mattbg"
+    useInsta(userid)
+
+if __name__ == "__main__":
+    main()
