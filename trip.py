1e71e6cff21073efcbdf2877eb16c4f45b7c73fc
atoregozh <toregozh@gmail.com>
2015-03-07 01:23:05 -0500

    mod trip

--- trip.py
+++ trip.py
@@ -7,12 +7,15 @@ import requests
 
 def getattractions(lat, lon, name):
     name.replace (" ", "+")
-    url = "http://api.tripadvisor.com/api/partner/2.0/map/" + str(lat) + "," + str(lon) + "," + "/attractions?key=HackTripAdvisor-ade29ff43aed&q=" + str(name)    
-    r = requests.get(url)
-    print r.text
+    url = "http://api.tripadvisor.com/api/partner/2.0/map/" + str(lat) + "," + str(lon) + "," + "/attractions?key=HackTripAdvisor-ade29ff43aed&q=" + name   
+    respose = requests.get(url)
+    
+    if response.status_code == 200:
+        return json.loads(response.text)
+    return None
 
 lat = 40.768924369
 lon = -73.975306691
-nam = Central Park
+nam = "Central Park"
 
 getattractions(lat,lon,nam)
